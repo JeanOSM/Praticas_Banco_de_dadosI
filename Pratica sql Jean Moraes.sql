d876dbb78744ce5d1929b3047dec52fd4ee2709d
JeanOSM <106047121+JeanOSM@users.noreply.github.com>
2022-08-22 20:46:00 -0300

    Atividade 01 BD.
    Primeira atividade pratica na criação de Base de dados.

---
+++ Pratica sql Jean Moraes.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE centro_de_distribuicao_data_base; /**COMANDO PARA CRIAR A BASE DE DADOS**/
+DROP DATABASE centro_de_distribuicao_data_base; /** COMANDO PARA EXCLUIR A BASE DE DADOS**/
+CREATE DATABASE centro_de_distribuicao; /**COMANDO PARA CRIAR A BASE DE DADOS**/
+USE centro_de_distribuicao; /**COMANDO PARA USAR O BANCO DE DADOS CRIADO*/
+DROP DATABASE IF EXISTS centro_de_distribuicao; /**COMANDO CASO EXISTA A BASE DE DADOS EXCLUI.**/
