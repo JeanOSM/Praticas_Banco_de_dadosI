936aa324a1c90ba49c94584818a7193849f9ddc7
JeanOSM <106047121+JeanOSM@users.noreply.github.com>
2022-08-29 20:58:18 -0300

    Aula29/08

--- Pratica sql Jean Moraes.sql
+++ Pratica sql Jean Moraes.sql
@@ -12,3 +12,37 @@ id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT
 , nome VARCHAR (100) NOT NULL 
 , sigla CHAR (2)  
 );
+
+
+Aula 29/08
+
+
+CREATE TABLE estado  (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nome_estado VARCHAR(255) NOT NULL,
+sigla CHAR(2) NOT NULL UNIQUE
+);
+
+INSERT INTO estado (nome_estado, sigla) VALUES ('Parana', 'PR');
+INSERT INTO estado (nome_estado, sigla) VALUES ('São Paulo', 'SP');
+INSERT INTO estado (nome_estado, sigla) VALUES ('Minas Gerais', 'MG');
+
+CREATE TABLE cidade  (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nome_cidade VARCHAR(255) NOT NULL,
+estado_id INT NOT NULL,
+FOREIGN KEY (estado_id) REFERENCES estado (id)
+);
+
+INSERT INTO cidade (nome_cidade, estado_id) VALUES ('Paranavaí', 1);
+INSERT INTO cidade (nome_cidade, estado_id) VALUES ('Jundiaí', 2);
+INSERT INTO cidade (nome_cidade, estado_id) VALUES ('Uberaba', 3);
+
+CREATE TABLE endereco  (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nome_rua VARCHAR(255) NOT NULL,
+cep VARCHAR(15) NOT NULL,
+numero INT NOT NULL,
+cidade_id INT NOT NULL,
+FOREIGN KEY (cidade_id) REFERENCES cidade (id)
+);
